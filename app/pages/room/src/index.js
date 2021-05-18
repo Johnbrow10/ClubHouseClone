51730450a2fb2a3501cd0b3f7f10596d048c96a5
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-05-18 17:00:29 -0300

    Arquivo de index para tela de room

---
+++ app/pages/room/src/index.js
@@ -0,0 +1,27 @@
+import { constants } from "../../_shared/constants.js";
+import SocketBuilder from "../../_shared/sockeBuilder.js";
+
+
+// Trazendo a importcao d oservidor na sala
+const socketBuilder = new SocketBuilder({
+  socketUrl: constants.socketUrl,
+  namespace: constants.socketNamespaces.room,
+});
+// estabelecendo a conexao com servidor
+const socket = socketBuilder
+  .setOnUserConnected((user) => console.log("user connected!", user))
+  .setOnUserDisconnected((user) => console.log("user disconnected!", user))
+  .build();
+
+socket.emit(constants.events.JOIN_ROOM, { user, room });
+
+const room = {
+  id: Date.now(),
+  topic: "JS Expert éh noix",
+};
+
+const user = {
+  img: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png",
+  username: "Johnbrow",
+};
+//  evento join-room
