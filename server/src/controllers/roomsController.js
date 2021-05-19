41fdd6e685abf4315386f3b44e3312888414b497
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-05-19 20:04:57 -0300

    function pedir autorização quando entrar na sala

--- server/src/controllers/roomsController.js
+++ server/src/controllers/roomsController.js
@@ -1,17 +1,45 @@
+import Attendee from "../entities/attendee.js";
 import { constants } from "../util/constants.js";
 
 export default class RoomsController {
-  constructor() {}
+  #users = new Map();
+  constructor() {
+    this.rooms = new Map();
+  }
 
   onNewConnection(socket) {
     const { id } = socket;
     console.log("connection stabelished with", id);
+    this.#updateGlobalUserData(id);
   }
 
-  joinRoom(socket, data) {
-    console.log("dados recebidos", data);
+  joinRoom(socket, { user, ...room }) {
+    const userId = (user.id = socket.id);
+    const roomId = room.id;
+
+    const updateUserData = this.#updateGlobalUserData(userId, user, roomId);
+
+    console.log({updateUserData});
 
     socket.emit(constants.event.USER_CONNECTED, data);
+  }
+
+  // function para pedir autorização quando entrar na sala
+  #updateGlobalUserData(userId, userData = {}, roomId = "") {
+    const user = this.#users.get(userId) ?? {};
+    const existingRoom = this.rooms.has(roomId);
+
+    const updateUserData = new Attendee({
+      ...user,
+      ...userData,
+      roomId,
+      // se for o unico na sala sempre sera o speaker
+      isSpeaker: !existingRoom,
+    });
+
+    this.#users.set(userId, updateUserData);
+
+    return this.#users.get(userId);
   }
 
   //  capturar as funcoes publicas e privadas da classe
