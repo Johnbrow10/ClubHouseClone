408eb428569cf62e3deb7f993a9a6bed9f0db43b
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-05-19 18:59:30 -0300

    eventos criacao de rotas com acao do userConnected

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,8 @@
 import RoomsController from "./controllers/roomsController.js";
 import SocketServer from "./util/socket.js";
 import Event from "events";
-
+import { constants } from "./util/constants.js";
+constants;
 const port = process.env.PORT || 3000;
 const socketServer = new SocketServer({ port });
 const server = await socketServer.start();
@@ -14,5 +15,31 @@ const namespaces = {
     eventEmitter: new Event(),
   },
 };
+
+//  Toda vez que haver uma conexao nova de um usuario
+// ele ira razer uma funcao de nova Conexao
+// namespaces.room.eventEmitter.on(
+//   "userConnected",
+//   namespaces.room.controller.onNewConnection.bind(namespaces.room.controller)
+// );
+
+// namespaces.room.eventEmitter.emit("userConnected", { id: "001" });
+// namespaces.room.eventEmitter.emit("userConnected", { id: "002" });
+// namespaces.room.eventEmitter.emit("userConnected", { id: "003" });
+
+const routeConfig = Object.entries(namespaces).map(
+  ([namespaces, { controller, eventEmitter }]) => {
+    const controllerEvents = controller.getEvents();
+    eventEmitter.on(
+      constants.event.USER_CONNECTED,
+      controller.onNewConnection.bind(controller)
+    );
+    return {
+      [namespaces]: { events: controllerEvents, eventEmitter },
+    };
+  }
+);
+
+socketServer.attachEvents({ routeConfig });
 
 console.log("socket server is running at", server.address().port);
